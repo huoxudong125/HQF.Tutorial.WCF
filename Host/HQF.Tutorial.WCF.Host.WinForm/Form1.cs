08a460f5259425ad6f75f0a182509f3e864d0953
huoxudong125 <423huo@163.com>
2015-08-19 10:09:52 +0800

    Add Hosts(Winform & Window Service)

---
+++ Host/HQF.Tutorial.WCF.Host.WinForm/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ServiceModel;
+using System.Windows.Forms;
+
+namespace HQF.Tutorial.WCF.Host.WinForm
+{
+    public partial class Form1 : Form
+    {
+        private bool _isRunning;
+        private ServiceHost host;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!_isRunning)
+            {
+                host = new ServiceHost(typeof(HQF.Tutorial.WCF.Contract.CalculatorService));
+
+                try
+                {
+                    host.Open();
+                    textBox1.Text += string.Format("The Sever has started.") + Environment.NewLine;
+                    _isRunning = true;
+                    button1.Text = "Stop";
+                }
+                catch (Exception ex)
+                {
+                    textBox1.Text += string.Format("Error:{0}", ex.Message) + Environment.NewLine;
+                }
+            }
+            else
+            {
+                try
+                {
+                    ((IDisposable)host).Dispose();
+                    textBox1.Text += string.Format("The Sever has stopped.") + Environment.NewLine;
+                    _isRunning = false;
+                    button1.Text = "Start";
+                }
+                catch (CommunicationObjectFaultedException ex)
+                {
+                    // TODO: add code to log
+                    textBox1.Text += string.Format("Error:{0}", ex.Message) + Environment.NewLine;
+                }
+            }
+        }
+    }
+}
---
+++ Host/HQF.Tutorial.WCF.Host.WinService/DemoService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Threading;
+using HQF.Tutorial.WCF.Contract;
+
+namespace HQF.Tutorial.WCF.Host.WinService
+{
+    public partial class DemoService : ServiceBase
+    {
+        private ServiceHost _host;
+
+        public DemoService()
+        {
+            InitializeComponent();
+        }
+
+        private void StopListening()
+        {
+            try
+            {
+                ((IDisposable) _host).Dispose();
+            }
+            catch (CommunicationObjectFaultedException ex)
+            {
+                // TODO: add code to log
+            }
+        }
+
+        #region override
+
+        protected override void OnStart(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(StartListening, this);
+        }
+
+        private static void StartListening(object state)
+        {
+            var service = state as DemoService;
+            if (service != null)
+            {
+                service._host = new ServiceHost(typeof (CalculatorService));
+                service._host.Faulted +=
+                    (s, e) =>
+                    {
+                        service.StopListening();
+                        service._host = null;
+                        StartListening(service);
+                    };
+            }
+        }
+
+        protected override void OnStop()
+        {
+            StopListening();
+        }
+
+        protected override void OnPause()
+        {
+            StopListening();
+            base.OnPause();
+        }
+
+        protected override void OnContinue()
+        {
+            ThreadPool.QueueUserWorkItem(StartListening, this);
+            base.OnContinue();
+        }
+
+        #endregion
+    }
+}
