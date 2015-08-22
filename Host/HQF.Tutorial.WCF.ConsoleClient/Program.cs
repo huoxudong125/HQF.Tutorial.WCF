31be2999af419770daa2fb9874678572a3b55dac
huoxudong125 <423huo@163.com>
2015-08-22 23:41:33 +0800

    Add WebHost for WCF Service..

--- Host/HQF.Tutorial.WCF.ConsoleClient/Program.cs
+++ Host/HQF.Tutorial.WCF.ConsoleClient/Program.cs
@@ -7,15 +7,26 @@ namespace HQF.Tutorial.WCF.ConsoleClient
     {
         private static void Main(string[] args)
         {
-            using (var proxy = new CalculatorServiceClient())
+            try
             {
-                Console.WriteLine("x + y = {2} when x = {0} and y = {1}", 1, 2, proxy.Add(1, 2));
-                Console.WriteLine("x - y = {2} when x = {0} and y = {1}", 1, 2, proxy.Subtract(1, 2));
-                Console.WriteLine("x * y = {2} when x = {0} and y = {1}", 1, 2, proxy.Multiply(1, 2));
-                Console.WriteLine("x / y = {2} when x = {0} and y = {1}", 1, 2, proxy.Divide(1, 2));
+                using (var proxy = new CalculatorServiceClient())
+                {
+                    Console.WriteLine("x + y = {2} when x = {0} and y = {1}", 1, 2, proxy.Add(1, 2));
+                    Console.WriteLine("x - y = {2} when x = {0} and y = {1}", 1, 2, proxy.Subtract(1, 2));
+                    Console.WriteLine("x * y = {2} when x = {0} and y = {1}", 1, 2, proxy.Multiply(1, 2));
+                    Console.WriteLine("x / y = {2} when x = {0} and y = {1}", 1, 2, proxy.Divide(1, 2));
 
-                Console.Read();
+                    
+                }
             }
+            catch (Exception ex)
+            {
+                
+               Console.WriteLine("Error :\n {0}",ex.Message);
+            }
+
+            Console.Read();
+
         }
     }
 }
