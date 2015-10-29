dca9ecb1c047e4781f6ed03ef8e8f0ec5ee08091
huoxudong125 <423huo@163.com>
2015-10-29 16:53:09 +0800

    Fix MSMQ_Mail Projects.

--- Src/Binding/MSMQ_Mail/Binding.MSMQ.Mail.Client.WPF/MainWindow.xaml.cs
+++ Src/Binding/MSMQ_Mail/Binding.MSMQ.Mail.Client.WPF/MainWindow.xaml.cs
@@ -4,12 +4,12 @@ using Binding.MSMQ.Mail.Client.WPF.ViewModel;
 namespace Binding.MSMQ.Mail.Client.WPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         /// <summary>
-        /// Initializes a new instance of the MainWindow class.
+        ///     Initializes a new instance of the MainWindow class.
         /// </summary>
         public MainWindow()
         {
--- Src/Binding/MSMQ_Mail/Binding.MSMQ.Mail.Client.WPF/ViewModel/ViewModelLocator.cs
+++ Src/Binding/MSMQ_Mail/Binding.MSMQ.Mail.Client.WPF/ViewModel/ViewModelLocator.cs
@@ -9,12 +9,12 @@
   DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
 */
 
+using Binding.MSMQ.Mail.Client.WPF.Model;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
 using Microsoft.Practices.ServiceLocation;
-using Binding.MSMQ.Mail.Clien.WPF.Model;
 
-namespace Binding.MSMQ.Mail.Clien.WPF.ViewModel
+namespace Binding.MSMQ.Mail.Client.WPF.ViewModel
 {
     /// <summary>
     /// This class contains static references to all the view models in the
